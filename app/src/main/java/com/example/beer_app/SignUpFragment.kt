bfc73a5986293946fd9a20f7463293b7ac8e8346
Marek Varis <marekvarisella@onet.pl>
2025-01-30 22:21:13 +0100

    Added signUp, accout fragments, Ui update + fix
    Added: accout fragment where you can navigate to login/signUp, map is now is zoomed in on poland, fixed filtr animation, changed some Ui fragments.

--- app/src/main/java/com/example/beer_app/SignUpFragment.kt
+++ app/src/main/java/com/example/beer_app/SignUpFragment.kt
@@ -3,28 +3,33 @@ package com.example.beer_app
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.TextView
+import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 
-class LoginFragment : Fragment() {
+
+class SignUpFragment : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        val  rootView = inflater.inflate(R.layout.fragment_login, container, false)
-        val goToSignUpButton = rootView.findViewById<TextView>(R.id.go_to_sign_up_button)
 
-        goToSignUpButton.setOnClickListener(){
-            rootView.findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+        val rootView = inflater.inflate(R.layout.fragment_sign_up, container, false)
+        val goToSignUpButton = rootView.findViewById<Button>(R.id.go_to_login_button)
+        goToSignUpButton?.setOnClickListener {
+            rootView.findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
         }
 
         return rootView
@@ -34,7 +39,7 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val rootLayout = view.findViewById<ConstraintLayout>(R.id.login_layout)
+        val rootLayout = view.findViewById<ConstraintLayout>(R.id.signup_layout)
         rootLayout.setOnTouchListener { _, _ ->
             hideKeyboard()
             true
@@ -43,8 +48,7 @@ class LoginFragment : Fragment() {
 
 }
 
-private fun LoginFragment.hideKeyboard() {
+private fun SignUpFragment.hideKeyboard() {
     val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(view?.windowToken, 0)
 }
-
