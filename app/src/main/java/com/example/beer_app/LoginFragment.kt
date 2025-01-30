624e6226de1d03e045fd5041f7c4a440ec750367
Marek Varis <marekvarisella@onet.pl>
2025-01-30 23:17:38 +0100

    Working in progress Login system.
    Just passing to main.

--- app/src/main/java/com/example/beer_app/LoginFragment.kt
+++ app/src/main/java/com/example/beer_app/LoginFragment.kt
@@ -2,15 +2,20 @@ package com.example.beer_app
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.android.material.textfield.TextInputEditText
+import com.google.gson.Gson
 
 class LoginFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,13 +26,36 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         val  rootView = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val loginButton = rootView.findViewById<Button>(R.id.login_button)
+        val usernameInput = rootView.findViewById<TextInputEditText>(R.id.username_input)
+        val passwordInput = rootView.findViewById<TextInputEditText>(R.id.password_input)
         val goToSignUpButton = rootView.findViewById<TextView>(R.id.go_to_sign_up_button)
 
-        goToSignUpButton.setOnClickListener(){
+        loginButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            if (validation(username, password)) {
+                Toast.makeText(requireContext(), "Login Successful!", Toast.LENGTH_SHORT).show()
+
+            }
+            else {
+                Toast.makeText(requireContext(), "Invalid username or password!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        goToSignUpButton.setOnClickListener {
             rootView.findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
         }
 
         return rootView
+    }
+
+
+    fun validation(username: String, password: String): Boolean {
+        val users = UserStorage.loadUsers(requireContext())
+        return users.any { it.username == username && it.password == password }
     }
 
     @SuppressLint("ClickableViewAccessibility")
--- app/src/main/java/com/example/beer_app/SignUpFragment.kt
+++ app/src/main/java/com/example/beer_app/SignUpFragment.kt
@@ -2,6 +2,7 @@ package com.example.beer_app
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,12 +10,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.findNavController
+import com.google.android.material.textfield.TextInputEditText
+import com.google.gson.Gson
 
 
 class SignUpFragment : Fragment() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,10 +29,33 @@ class SignUpFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val rootView = inflater.inflate(R.layout.fragment_sign_up, container, false)
-        val goToSignUpButton = rootView.findViewById<Button>(R.id.go_to_login_button)
-        goToSignUpButton?.setOnClickListener {
+
+        val signUpButton = rootView.findViewById<Button>(R.id.signup_button)
+        val usernameInput = rootView.findViewById<TextInputEditText>(R.id.username_input)
+        val passwordInput = rootView.findViewById<TextInputEditText>(R.id.password_input)
+        var goToLoginButton = rootView.findViewById<TextView>(R.id.go_to_login_button)
+
+        signUpButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                val newUser = User(username, password)
+
+                if (UserStorage.addUser(requireContext(), newUser)) {
+                    Toast.makeText(requireContext(), "User Registered Successfully!", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    Toast.makeText(requireContext(), "Username already exists!", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else {
+                Toast.makeText(requireContext(), "Please enter both username and password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        goToLoginButton?.setOnClickListener {
             rootView.findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
         }
 
